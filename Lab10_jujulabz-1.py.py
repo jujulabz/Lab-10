4a3e7f56f85f4fc630f8157840ef6941d57a52e8
jujulabz <mp678503@gmail.com>
2026-03-29 18:42:44 -0400

    Started work on the word analyzer class and added in the 4 diffrent files

---
+++ Lab10_jujulabz-1.py.py
@@ -0,0 +1,14 @@
+"""
+Word Count
+Melissa Palmer
+This program presents a menu of 4 predefined files and once the user selects the file they want the program will anaylze the file.
+Starter code came from 
+3/29/2026
+"""
+
+import pathlib
+
+class WordAnalyzer:
+    
+    def __init__(self, filepath):
+        
